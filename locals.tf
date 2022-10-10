5e583cad9a59db12927dbfe325bb90fd66ad5fbb
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-11 01:12:08 +0530

    added docdb

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   username  = element(split("/", data.aws_ssm_parameter.credentials.value), 0)
   password  = element(split("/", data.aws_ssm_parameter.credentials.value), 1)
-  vpc_id    = ./vendor/module.vpc.out["apps"]["out"][*].vpc_id
+  vpc_id    = /vendor/module.vpc.out["apps"]["out"][*].vpc_id
 }
 
