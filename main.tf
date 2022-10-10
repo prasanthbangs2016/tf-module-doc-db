1c524f8f19a3a2f22df6d74e8b812c187686548e
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-11 00:45:44 +0530

    added docdb

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ resource "aws_docdb_cluster" "docdb" {
 #  preferred_backup_window = "07:00-09:00"
   #when u try to delete cluster it will delete automatically  with kind of back up
   skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.main.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
 
   tags = {
     Name = "Roboshop-${var.env}"
@@ -16,12 +19,47 @@ resource "aws_docdb_cluster" "docdb" {
 }
 #creating cluster in db subnets
 
-#resource "aws_docdb_subnet_group" "default" {
-#  name       = "main"
-#  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
-#
-#  tags = {
-#    Name = "Roboshop-${var.env}"
+resource "aws_docdb_subnet_group" "main" {
+  name       = "roboshop-${var.env}"
+  subnet_ids = var.apps_subnet_ids
+
+  tags = {
+    Name = "Roboshop-${var.env}"
+  }
+}
+
+#creating db configuration using parameter group
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb4.0"
+  name        = "Roboshop-${var.env}"
+  description = "Roboshop-${var.env}"
+
+#  parameter {
+#    name  = "tls"
+#    value = "enabled"
 #  }
-#}
+}
+
+#creating security group for db
+
+resource "aws_security_group" "docdb" {
+  name        = "Roboshop-${var.env}-docdb"
+  description = "Roboshop-${var.env}-docdb"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "docdb"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr_block]
+
+  }
+
+
+  tags = {
+    Name = "Roboshop-${var.env}-docdb"
+  }
+}
 
