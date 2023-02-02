ba48350905b7dcc8b00e48e8da4df4d06871e832
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 00:17:27 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -75,8 +75,8 @@ resource "null_resource" "mongo_schema_apply" {
     command = <<EOF
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
-unzip mongodb.zip
-cd mysql-main
+unzip -o mongodb.zip
+cd mongodb-main
 curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${local.username} --password ${local.password} <catalogue.js
 mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${local.username} --password ${local.password} <users.js
