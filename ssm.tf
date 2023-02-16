10419d7cd30587a94741d250c72ac5389f20231b
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-17 00:23:57 +0530

    added tf file

--- ssm.tf
+++ ssm.tf
@@ -1,12 +1,12 @@
 resource "aws_ssm_parameter" "docdb-url-catalogue" {
-  name  = "'mutable.docdb.catalogue.${var.env}.MONGO_URL"
+  name  = "mutable.docdb.catalogue.${var.env}.MONGO_URL"
   type  = "String"
   value = "mongodb://${local.username}:${local.password}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
 
 
 resource "aws_ssm_parameter" "docdb-url-users" {
-  name  = "'mutable.docdb.user.${var.env}.MONGO_URL"
+  name  = "mutable.docdb.user.${var.env}.MONGO_URL"
   type  = "String"
   value = "mongodb://${local.username}:${local.password}@${aws_docdb_cluster.docdb.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
