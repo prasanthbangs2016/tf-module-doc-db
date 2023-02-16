3e4b99d328109b564e60309e4e8f99268827312b
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-16 23:55:54 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -86,11 +86,7 @@ EOF
   }
 }
 
-resource "aws_ssm_parameter" "docdb-url" {
-  name  = "'mutable.docdb.{ENV}.MONGO_URL"
-  type  = "String"
-  value = "mongodb://${local.username}:${local.password}@roboshop-dev.cluster-cjsux0hozm9j.us-east-1.docdb.amazonaws.com:27017/catalogue?"
-}
+
 
 
 
