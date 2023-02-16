527dce8f74ca1caf05476893e2e9717311a62253
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-17 00:13:32 +0530

    added tf file

--- ssm.tf
+++ ssm.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "docdb-url-catalogue" {
 
 
 resource "aws_ssm_parameter" "docdb-url-users" {
-  name  = "'mutable.docdb.catalogue.${var.env}.MONGO_URL"
+  name  = "'mutable.docdb.user.${var.env}.MONGO_URL"
   type  = "String"
   value = "mongodb://${local.username}:${local.password}@roboshop-dev.cluster-cjsux0hozm9j.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
